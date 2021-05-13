aa9ac16b00c30ec541a17e1d9b38c39fc0d3ec76
Miguel Nogueira <mglsndc@gmail.com>
2021-05-13 15:48:28 -0300

    hiding firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 const firebase = require('firebase')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC-spp0wrAjnpPAvOM61azjQuBvqk01uus',
-  authDomain: 'testi-2f86d.firebaseapp.com',
-  projectId: 'testi-2f86d',
-  storageBucket: 'testi-2f86d.appspot.com',
-  messagingSenderId: '814013455525',
-  appId: '1:814013455525:web:e1d924b213b41f0628814c',
+  apiKey: 'api key',
+  authDomain: 'auth domain',
+  projectId: 'project id',
+  storageBucket: 'bucket',
+  messagingSenderId: 'sender id',
+  appId: 'app id',
 }
 
 if (!firebase.apps.length) {
